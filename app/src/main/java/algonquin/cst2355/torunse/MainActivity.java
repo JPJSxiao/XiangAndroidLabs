77af93ed88c6485c3638e2672ca6bfff405fc30d
Xiangwu <dai00048@algonquinlive.com>
2023-01-18 16:09:27 -0500

    Commit 4 using called viewBinding

--- app/src/main/java/algonquin/cst2355/torunse/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/torunse/MainActivity.java
@@ -3,12 +3,43 @@ package algonquin.cst2355.torunse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import algonquin.cst2355.torunse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+        TextView mytext = variableBinding.textview;
+
+//        TextView mytext = findViewById(R.id.textview);
+//        Button button = findViewById(R.id.mybutton);
+//        EditText myedit = findViewById(R.id.myedittext);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//               String editString = myedit.getText().toString();
+//                mytext.setText( "Your edit text has: " + editString);
+          variableBinding.mybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+               String editString = variableBinding.myedittext.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+
+            }
+        });
+
     }
+
 }
