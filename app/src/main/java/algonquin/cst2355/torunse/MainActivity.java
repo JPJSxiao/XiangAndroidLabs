b507cdf6a062a2f916bc744dd081f849d7cd3bdd
Xiangwu <dai00048@algonquinlive.com>
2023-01-18 23:25:13 -0500

    add a ImageView and a ImageButton

--- app/src/main/java/algonquin/cst2355/torunse/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/torunse/MainActivity.java
@@ -1,26 +1,67 @@
 package algonquin.cst2355.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import algonquin.cst2355.torunse.data.MainActivityViewModel;
 import algonquin.cst2355.torunse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding variableBinding;
+    private MainActivityViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         //setContentView(R.layout.activity_main);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
-        TextView mytext = variableBinding.textview;
+       // TextView mytext = variableBinding.textview;
+        viewModel.isSelected.observe(this, newBooleanValue ->{
+            variableBinding.switch1.setChecked(newBooleanValue);
+            variableBinding.checkBox.setChecked(newBooleanValue);
+            variableBinding.radioButton.setChecked(newBooleanValue);
+        });
+        variableBinding.radioButton.setChecked(viewModel.isSelected.getValue());
+        variableBinding.switch1.setChecked(viewModel.isSelected.getValue());
+        variableBinding.checkBox .setChecked(viewModel.isSelected.getValue());
+
+
+       variableBinding.switch1.setOnCheckedChangeListener((whichButton, isChecked)->{
+           viewModel.isSelected.postValue(isChecked);
+
+           Toast
+                   .makeText(MainActivity.this, "the boolean is now:" +isChecked, Toast.LENGTH_SHORT)
+                   .show();
+        });
+
+        variableBinding.radioButton.setOnCheckedChangeListener((whichButton, isChecked)->{
+            viewModel.isSelected.postValue(isChecked);
+
+            Toast
+                    .makeText(MainActivity.this, "the boolean is now:" +isChecked, Toast.LENGTH_SHORT)
+                    .show();
+        });
+
+        variableBinding.checkBox.setOnCheckedChangeListener((whichButton, isChecked)->{
+            viewModel.isSelected.postValue(isChecked);
+
+            Toast
+                    .makeText(MainActivity.this, "the boolean is now:" +isChecked, Toast.LENGTH_SHORT)
+                    .show();
+        });
+
+        variableBinding.textview.setText("Your edit text has: " + viewModel.editTextContents);
 
 //        TextView mytext = findViewById(R.id.textview);
 //        Button button = findViewById(R.id.mybutton);
@@ -31,15 +72,23 @@ public class MainActivity extends AppCompatActivity {
 //                // Code here executes on main thread after user presses button
 //               String editString = myedit.getText().toString();
 //                mytext.setText( "Your edit text has: " + editString);
-          variableBinding.mybutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+//          variableBinding.mybutton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+        variableBinding.mybutton.setOnClickListener( click -> {
                 // Code here executes on main thread after user presses button
-               String editString = variableBinding.myedittext.getText().toString();
-                mytext.setText( "Your edit text has: " + editString);
-
-            }
+//               String editString = variableBinding.myedittext.getText().toString();
+//                mytext.setText( "Your edit text has: " + editString);
+               // viewModel.editTextContents = variableBinding.myedittext.getText().toString();
+               // variableBinding.textview.setText("Your edit text has: " + viewModel.editTextContents);
+        //    }
+            viewModel.editTextContents.postValue(variableBinding.myedittext.getText().toString());
         });
 
+        variableBinding.myimagebutton.setOnClickListener(imageBtn -> {
+            Toast
+                    .makeText(MainActivity.this,  "The width = " + variableBinding.myimagebutton.getWidth() + " and height = " + variableBinding.myimagebutton.getHeight(), Toast.LENGTH_SHORT)
+                    .show();
+        });
     }
 
 }
---
+++ app/src/main/java/algonquin/cst2355/torunse/data/MainActivityViewModel.java
@@ -0,0 +1,12 @@
+package algonquin.cst2355.torunse.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+    //Holds a string , but notifies observers when changed
+    public MutableLiveData<String> editTextContents = new MutableLiveData<>();
+
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>(false);
+}
